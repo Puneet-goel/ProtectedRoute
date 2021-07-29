be6750b490dffc28cd3af2a55d5f7a2b23afe1c7
Puneet-goel <puneetgoel016@gmail.com>
2021-07-29 21:41:53 +0530

    added user data

---
+++ src/api/api.js
@@ -0,0 +1,24 @@
+{
+	[
+	    {
+	    	"email": "user1@gmail.com",
+	    	"password": "firewall"
+	    },
+	    {
+	    	"email": "user2@yahoo.com",
+	    	"password": "password123"
+	    },
+	    {
+	    	"email": "user3@gmail.com",
+	    	"password": "123456"
+	    },
+	    {
+	    	"email": "user4@gmail.com",
+	    	"password": "chocolate"
+	    },
+	    {
+	    	"email": "user5@yahoo.com",
+	    	"password": "hello"
+	    }
+	]
+}
